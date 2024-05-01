26f741e4e529d6fe72b9071440afeed67a9d7d90
Ally <allyharrison8393@gmail.com>
2024-05-01 18:46:57 +0800

    Fixed broken map, modal bugs, dropdown toggler. Added autoformatter.

--- static/JS.js
+++ static/JS.js
@@ -1,41 +1,29 @@
-
 // Just commenting the map out for now as it was causing an error
-
-// please don't span api :( over a threshold and I pay
-// mapboxgl.accessToken = 'pk.eyJ1Ijoic3RldmkiLCJhIjoiY2x2ZWtrdThhMGI1bjJpbnFrNm9xem80YSJ9.Lz5tsAHEt_qZED_2_wyEGw';
-// const map = new mapboxgl.Map({
-//     container: 'map',
-//     // Choose from Mapbox's core styles, or make your own style with Mapbox Studio
-//     style: 'mapbox://styles/mapbox/streets-v12',
-//     center: [116, -31.9],
-//     zoom: 10
-// });
 
 // please don't span api :( over a threshold and I pay\
 // JS code adapted from MapBox tutorials and chatgpt help
-mapboxgl.accessToken = 'pk.eyJ1Ijoic3RldmkiLCJhIjoiY2x2ZWtrdThhMGI1bjJpbnFrNm9xem80YSJ9.Lz5tsAHEt_qZED_2_wyEGw';
+mapboxgl.accessToken =
+    'pk.eyJ1Ijoic3RldmkiLCJhIjoiY2x2ZWtrdThhMGI1bjJpbnFrNm9xem80YSJ9.Lz5tsAHEt_qZED_2_wyEGw' // TODO - move this to .env file
 const map = new mapboxgl.Map({
     container: 'map',
     // Choosen from Mapbox's core styles
     style: 'mapbox://styles/mapbox/streets-v12',
     center: [116, -31.9],
-    zoom: 10
-});
+    zoom: 10,
+})
 
-
-let start = [116, -31.9]; // Default start location
+let start = [116, -31.9] // Default start location
 
 // Add the control to the map.
-// map.addControl(
-//     new MapboxGeocoder({
-//         accessToken: mapboxgl.accessToken,
-//         mapboxgl: mapboxgl
-//     })
-// );
-
+map.addControl(
+    new MapboxGeocoder({
+        accessToken: mapboxgl.accessToken,
+        mapboxgl: mapboxgl,
+    })
+)
 
 // document.addEventListener('DOMContentLoaded', function() {
-//   //Login forms 
+//   //Login forms
 //   const loginForm = document.getElementById("login-form");
 //   const loginButton = document.getElementById("login-form-submit");
 //   const loginErrorMsg = document.getElementById("login-error-msg");
@@ -60,21 +48,21 @@ let start = [116, -31.9]; // Default start location
 // });
 
 // This is half working, the modal pops up but the static background doesn't disappear when I close it
-//  
+//
 // This is to merge the exisitng login JS with a new event listener
 // It shows the login modal when clicked from any page
 
 map.addControl(
     new MapboxGeocoder({
         accessToken: mapboxgl.accessToken,
-        mapboxgl: mapboxgl
+        mapboxgl: mapboxgl,
     })
-);
+)
 
 // Function to update the start location and fetch the route
 async function updateStartLocation(coords) {
-    start = coords;
-    await getRoute(start);
+    start = coords
+    await getRoute(start)
 }
 
 // create a function to make a directions request
@@ -83,56 +71,56 @@ async function getRoute(end) {
     const query = await fetch(
         `https://api.mapbox.com/directions/v5/mapbox/cycling/${start[0]},${start[1]};${end[0]},${end[1]}?steps=true&geometries=geojson&access_token=${mapboxgl.accessToken}`,
         { method: 'GET' }
-    );
-    const json = await query.json();
-    const data = json.routes[0];
-    const route = data.geometry.coordinates;
+    )
+    const json = await query.json()
+    const data = json.routes[0]
+    const route = data.geometry.coordinates
     const geojson = {
         type: 'Feature',
         properties: {},
         geometry: {
             type: 'LineString',
-            coordinates: route
-        }
-    };
+            coordinates: route,
+        },
+    }
     // if the route already exists on the map, we'll reset it using setData
     if (map.getSource('route')) {
-        map.getSource('route').setData(geojson);
+        map.getSource('route').setData(geojson)
     } else {
         map.addLayer({
             id: 'route',
             type: 'line',
             source: {
                 type: 'geojson',
-                data: geojson
+                data: geojson,
             },
             layout: {
                 'line-join': 'round',
-                'line-cap': 'round'
+                'line-cap': 'round',
             },
             paint: {
                 'line-color': '#3887be',
                 'line-width': 5,
-                'line-opacity': 0.75
-            }
-        });
+                'line-opacity': 0.75,
+            },
+        })
     }
     // Display turn instructions
-    const instructions = document.getElementById('instructions');
-    const steps = data.legs[0].steps;
+    const instructions = document.getElementById('instructions')
+    const steps = data.legs[0].steps
 
-    let tripInstructions = '';
+    let tripInstructions = ''
     for (const step of steps) {
-        tripInstructions += `<li>${step.maneuver.instruction}</li>`;
+        tripInstructions += `<li>${step.maneuver.instruction}</li>`
     }
     instructions.innerHTML = `<p><strong>Trip duration: ${Math.floor(
         data.duration / 60
-    )} min 🚴 </strong></p><ol>${tripInstructions}</ol>`;
+    )} min 🚴 </strong></p><ol>${tripInstructions}</ol>`
 }
 
 map.on('load', () => {
     // make an initial directions request that starts and ends at the same location
-    getRoute(start);
+    getRoute(start)
 
     // Add starting point to the map
     map.addLayer({
@@ -148,35 +136,24 @@ map.on('load', () => {
                         properties: {},
                         geometry: {
                             type: 'Point',
-                            coordinates: start
-                        }
-                    }
-                ]
-            }
+                            coordinates: start,
+                        },
+                    },
+                ],
+            },
         },
         paint: {
             'circle-radius': 10,
-            'circle-color': '#3887be'
-        }
-    });
+            'circle-color': '#3887be',
+        },
+    })
 
     // Listen for clicks on the map to update start location
     map.on('click', async (event) => {
-        const coords = Object.keys(event.lngLat).map((key) => event.lngLat[key]);
-        await updateStartLocation(coords);
-    });
-});
-
-document.addEventListener("DOMContentLoaded", function() {
-  const loginBtn = document.getElementById("loginBtn");
-  const loginModal = new bootstrap.Modal(document.getElementById('loginModal'));
-
-  // Add an event listener to the login button
-  loginBtn.addEventListener("click", function(event) {
-      event.preventDefault();
-      loginModal.show();
-  });
-});
+        const coords = Object.keys(event.lngLat).map((key) => event.lngLat[key])
+        await updateStartLocation(coords)
+    })
+})
 
 // Commenting this out for now as I haven't tested it with the frontend stuff that I've recently added
 /*
