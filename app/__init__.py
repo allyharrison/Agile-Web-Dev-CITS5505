2ab0e0b31828fddb6e5ff71c598025e07a57abf7
kartikbhalala <47134161+kartikbhalala@users.noreply.github.com>
2024-05-11 14:05:51 +0800

    update

--- app/__init__.py
+++ app/__init__.py
@@ -4,12 +4,15 @@ from flask_migrate import Migrate
 from flask_login import LoginManager
 from .config import Config
 from flask_mde import Mde
+from flask_moment import Moment
+
 
 app = Flask(__name__)
 mde = Mde(app)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+moment = Moment(app)
 
 login = LoginManager(app)
 login.login_view = 'login'
