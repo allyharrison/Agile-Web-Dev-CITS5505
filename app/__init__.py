f9fe9b8a714607d5a09b9f797500bf1a0cb53bae
kartikbhalala <47134161+kartikbhalala@users.noreply.github.com>
2024-05-18 15:29:05 +0800

    selenium testing and post modification

--- app/__init__.py
+++ app/__init__.py
@@ -8,7 +8,9 @@ from flask_moment import Moment
 from flask import request
 from flask_babel import Babel, lazy_gettext as _l
 from elasticsearch import Elasticsearch
-
+from markupsafe import Markup
+import re
+import bleach  # Import bleach for sanitization
 
 def get_locale():
     return request.accept_languages.best_match(app.config['LANGUAGES'])
@@ -24,10 +26,23 @@ babel = Babel(app, locale_selector=get_locale)
 login = LoginManager(app)
 login.login_view = 'login'
 login.login_message = _l('Please log in to access this page.')
-app.elasticsearch = Elasticsearch([app.config['ELASTICSEARCH_URL']]) \
-        if app.config['ELASTICSEARCH_URL'] else None
+app.elasticsearch = Elasticsearch([app.config['ELASTICSEARCH_URL']]) if app.config['ELASTICSEARCH_URL'] else None
+
+# Define the nl2br custom filter with sanitization and allowed HTML tags/attributes
+def nl2br(value):
+    allowed_tags = ['a', 'abbr', 'acronym', 'b', 'blockquote', 'code', 'em', 'i', 'li', 'ol', 'strong', 'ul', 'p', 'br', 'span', 'div']
+    allowed_attrs = {
+        '*': ['class', 'style'],
+        'a': ['href', 'title'],
+        'abbr': ['title'],
+        'acronym': ['title'],
+    }
+    clean_value = bleach.clean(value, tags=allowed_tags, attributes=allowed_attrs, strip=True)  # Sanitize the input
+    result = re.sub(r'(\r\n|\n\r|\r|\n)', r'<br>', clean_value)
+    return Markup(result)
+
+app.jinja_env.filters['nl2br'] = nl2br
 
 print("Elasticsearch URL:", app.config['ELASTICSEARCH_URL'])  # Add this line to debug
 
 from . import routes, models
-
