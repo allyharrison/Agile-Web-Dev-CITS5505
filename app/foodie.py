593136aae91ac4f99c22a66273609a946b9a7ad1
neha200796 <144754137+neha200796@users.noreply.github.com>
2024-05-18 19:37:38 +0800

    adding comments in foodie.py

--- app/foodie.py
+++ app/foodie.py
@@ -3,7 +3,10 @@ import sqlalchemy.orm as so
 from app import app, db
 from app.models import User, Post
 
+# Define a shell context processor function 
 @app.shell_context_processor
 def make_shell_context():
+    # Return a dictionary that includes SQLAlchemy core, ORM, database instance, and models
+    # This allows these objects to be automatically available in the Flask shell
     return {'sa': sa, 'so': so, 'db': db, 'User': User, 'Post': Post}
 
